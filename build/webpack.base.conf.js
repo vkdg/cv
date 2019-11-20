a7d57f8932ccb1720c808479fbdf265a1ae3317b
Denis Hardenberg <6463690+vkdg@users.noreply.github.com>
2019-11-20 16:14:53 +0500

    f

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -72,13 +72,13 @@ module.exports = {
         },
         {
           loader: 'css-loader',
-          options: { sourceMap: true }
+          options: { sourceMap: true, url: false }
         }, {
           loader: 'postcss-loader',
           options: { sourceMap: true, config: { path: `./postcss.config.js` } }
         }, {
           loader: 'sass-loader',
-          options: { sourceMap: true }
+          options: { sourceMap: true, sourceMapContents: false }
         }
       ]
     }, {
